9684e0d1523e145680cbe9c2462579c314684775
Daniel Roa <daroap@udistrital.edu.co>
2023-07-07 22:06:40 -0500

    llenando el modal con los datos del producto

--- scriptModal.js
+++ scriptModal.js
@@ -6,9 +6,39 @@ let detalles = document.getElementById("texto-detalles");
 let span = document.getElementsByClassName("close")[0];
 
 detalles.onclick = function() {
-    console.log(getSlideIndex());
-    
+
+  let tituloModal = document.getElementById("nombre_producto");  
+  tituloModal.textContent = productos[getSlideIndex()].nombre_completo;
+
+  let imagenModal = document.getElementById("image_modal");  
+  imagenModal.src = productos[getSlideIndex()].urlImage;
+
+  let marca = document.getElementById("marca");  
+  marca.textContent = productos[getSlideIndex()].marca;
+
+  let description = document.getElementById("description");  
+  description.textContent = productos[getSlideIndex()].description;
+
+  let caracteristicas = productos[getSlideIndex()].caracteristicas.split(';');
+  let lista = document.getElementById('lista_caracteristicas');
+
+  while (lista.hasChildNodes()) {
+    lista.removeChild(lista.firstChild);
+  }
+
+  for(let i = 0; i < caracteristicas.length ; i++){
+
+    let item = document.createElement('li');
+    item.textContent = caracteristicas[i];
+    lista.appendChild(item);
+
+  }
+
+  let precio = document.getElementById("precio");  
+  precio.textContent = productos[getSlideIndex()].precio;
+
   modal.style.display = "block";
+
 }
 
 span.onclick = function() {
